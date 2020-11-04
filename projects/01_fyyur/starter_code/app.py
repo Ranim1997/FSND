8c209f68dfdc08593ee7bcc17aac33d896f161fd
Ranim1997 <65516141+Ranim1997@users.noreply.github.com>
2020-11-04 14:31:30 +0300

    Update app.py

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -129,18 +129,23 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for Hop should return "The Musical Hop".
-  # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
+    
+    search = request.form.get("search_term", "")
+    search_result = {"count": 0, "data": []}
+
+    venue_id_name = ["id", "name"]
+    venue_search_results = (db.session.query(Venue).filter(Venue.name.ilike(f"%{search}%")).options(load_only(*venue_id_name)).all())
+
+    search_result["count"] = len(venue_search_results)
+
+    for result in venue_search_results:
+        venue_id_name = {
+            "id": result.id,
+            "name": result.name,
+        }
+        search_result["data"].append(venue_id_name)
+
+    return render_template("pages/search_venues.html", results=search_result, search_term=request.form.get("search_term", ""))
 
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
@@ -291,9 +296,7 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
+
   search= request.form.get("search_term", "")
   search_result = {"count": 0, "data": []}
   artist_id_name = ["id", "name"]
